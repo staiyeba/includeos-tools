5c54a09f659f9b1c34271681642b59a6a265985b
taiyeba <staiyeba@gmail.com>
2019-04-09 15:05:27 +0200

    puppet: adding file for building external dependencies

---
+++ puppet/build_external_dep.pp
@@ -0,0 +1,29 @@
+# Build system
+package { [ "cmake" , "make", "nasm", "libssl-dev", "libseccomp-dev", "gcc-multilib" ] :
+  ensure => present,
+}
+
+# Compilers
+package { [ "clang-6.0", "gcc-7", "g++-multilib" ] :
+  ensure => present,
+}
+
+# Build arm
+package { [ "gcc-aarch64-linux-gnu", "c++-aarch64-linux-gnu" ] :
+  ensure => present,
+}
+
+# Test system dependencies
+package { [ "qemu-system", "lcov", "grub2", "openjdk-8-jre-headless" ] :
+  ensure => present,
+}
+
+package { [ "python3-pip", "python3-setuptools", "python3-dev" ] :
+  ensure => present,
+}
+
+$pip_packages = [ "wheel", "jsonschema", "conan", "psutil", "ws4py" ]
+package { $pip_packages :
+  ensure => present,
+  provider => pip3,
+}
