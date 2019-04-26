ec7bb0c35f3d0511689d689f2630b6d81f464b28
taiyeba <staiyeba@gmail.com>
2019-04-26 11:06:32 +0200

    puppet: adding more specific puppet agent info

--- puppet/install_puppet_and_test_client.sh
+++ puppet/install_puppet_and_test_client.sh
@@ -6,6 +6,8 @@ wget https://apt.puppet.com/puppet6-release-bionic.deb
 sudo dpkg -i puppet6-release-bionic.deb
 sudo apt-get update
 
-sudo apt-get -y install puppet
+sudo apt-get -y install puppet-agent
+sudo echo "PATH=$PATH:/opt/puppetlabs/bin" >> ~/.bashrc
+source ~/.bashrc
 
 sudo puppet apply ~/includeos-tools/puppet/$FILE
